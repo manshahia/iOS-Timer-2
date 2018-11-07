3622891eaf82f28e41d637e55cec726f4c577fd9
Ravi Inder Manshahia <ravinder@lab91.co>
2018-11-07 10:22:38 +0530

    UI Complete

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func pausePressed(_ sender: Any) {
+    }
+    
+    @IBAction func playPressed(_ sender: Any) {
+    }
+    
+    @IBAction func addTenSeconds(_ sender: Any) {
+    }
+    
+    
+    @IBAction func removeTenSeconds(_ sender: Any) {
+    }
+    
+    @IBAction func resetPressed(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
