06dd615ac1336ffc03e3adc3ed408bf52f596fed
Ravi Inder Manshahia <ravinder@lab91.co>
2018-11-07 10:56:29 +0530

    LOGIC Complete

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -10,35 +10,80 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func pausePressed(_ sender: Any) {
-    }
+ 
     
-    @IBAction func playPressed(_ sender: Any) {
-    }
-    
-    @IBAction func addTenSeconds(_ sender: Any) {
-    }
-    
-    
-    @IBAction func removeTenSeconds(_ sender: Any) {
-    }
-    
-    @IBAction func resetPressed(_ sender: Any) {
-    }
+    //MARK: Outlet & Variables
     
     @IBOutlet weak var timerLabel: UILabel!
     
+    var timer = Timer()
+    
+    var time = 220
+    
+    //MARK: System boilerplate
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    //MARK: IBActions
+    @IBAction func pausePressed(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func playPressed(_ sender: Any) {
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true)
+        {
+            timer in
+            self.updateTime(time: -1 )
+            
+            
+            print("Timer countdown \(self.time)")
+            
+            if self.time == 0
+            {
+                self.timer.invalidate()
+            }
+        }
+    }
+    
+    @IBAction func addTenSeconds(_ sender: Any) {
+        updateTime(time: 10)
+    }
+    
+    
+    @IBAction func removeTenSeconds(_ sender: Any) {
+        updateTime(time: -10)
+    }
+    
+    @IBAction func resetPressed(_ sender: Any) {
+        
+        timer.invalidate()
+        resetTime(time: 220)
+        
+    }
 
+    //MARK: Utility Functions
+    func updateTimeLabel() {
+        self.timerLabel.text = String(self.time)
+    }
+    func updateTime(time : Int) {
+        self.time = self.time + time
+        updateTimeLabel()
+    }
+    func resetTime(time : Int)
+    {
+        self.time = time
+        updateTimeLabel()
+    }
 
 }
 
